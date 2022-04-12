5e2a88e147afa38f82ef398e59e081f24d8af8d4
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-04-12 20:26:27 -0300

    templates -> ok

--- autogit.sh
+++ autogit.sh
@@ -1,7 +1,4 @@
-git init
 git add .
 git commit -m "templates -> ok"
-git branch -M main
-git remote add origin https://github.com/CarlosViniMSouza/FlaskSQLChallenger.git
-git push -u origin main
+git push
 git status
